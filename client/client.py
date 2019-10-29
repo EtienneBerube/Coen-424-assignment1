cd8a592a05a6724165edccb84e5f58b9acb9da4f
Etienne <tiennebrb@gmail.com>
2019-10-29 15:39:43 -0400

    Finished client

--- client/client.py
+++ client/client.py
@@ -1,12 +1,13 @@
+import json
+import uuid
 
 import requests
-import random
-import uuid
+
 from data_packet_pb2 import ReturnPacket
 
 loop = True
 
-while loop :
+while loop:
     URL = "http://localhost:80/data/"
     print("Which product:\n1)DVD data\n2)NDBench data\n[ENTER NUMBER] ")
     product = int(input("Product: "))
@@ -39,7 +40,8 @@ while loop :
     print("Number of batches \n[ENTER NUMBER]")
     batchSize = int(input())
 
-    print("Which metric :\n1)CPU Utilization\n2)Network in\n3)Network out\n4)Memory utilization\n5)Final Target[ENTER NUMBER]")
+    print(
+        "Which metric :\n1)CPU Utilization\n2)Network in\n3)Network out\n4)Memory utilization\n5)Final Target[ENTER NUMBER]")
     metric = int(input("Data set: "))
 
     if metric == 1:
@@ -67,7 +69,6 @@ while loop :
         print("Bad input, start again \n")
         continue
 
-
     rfwId = uuid.uuid4().hex
 
     params = {
@@ -78,22 +79,27 @@ while loop :
         "batchSize": batchSize
     }
 
-    url = URL+product+'-'+set+'/'+protocol
+    url = URL + product + '-' + set + '/' + protocol
 
-    print(url)
-
-    print(params)
-
-
-
+    print('Sending to: ' + url)
 
     data = requests.get(url=url, params=params)
 
-    if(protocol == 'json'):
-        print(data.json)
+    if (protocol == 'json'):
+        print(json.dumps(data.json(), sort_keys=True, indent=4))
     elif protocol == 'proto':
         parsed_data = ReturnPacket()
         print(parsed_data.ParseFromString(data.text))
     else:
         print('Cannot read response')
 
+    redo = input('Start again? [y/n]: ')
+
+    if redo == 'y':
+        loop = True
+        print("=============== Next Batch ==============")
+    elif redo == 'n':
+        loop = False
+    else:
+        print('Unrecognized option, exiting')
+        break
